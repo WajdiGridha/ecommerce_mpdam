1dea232f8f7ce72a5611adaa4569e73262b50d0b
WajdiGridha <wajdigridha744@gmail.com>
2023-01-13 18:56:05 +0100

    fix userModel and some controllers

---
+++ backend/controllers/authController.js
@@ -0,0 +1,39 @@
+const User = require("../models/user");
+
+const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const user = await User.create({ name, email, password });
+    const token = user.createJWT();
+    res
+      .status(201)
+      .json({ user: { email: user.email, name: user.name }, token });
+  } catch (error) {
+    res.status(500).json({ msg: "an error occurred" });
+  }
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(500).json({ msg: "please provide all values" });
+  }
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    res.status(500).json({ msg: "Email/password Invalid" });
+  }
+
+  const isCorrect = await user.comparePassword(password);
+
+  if (!isCorrect) {
+    res.status(500).json({ msg: "Invalid credentials" });
+  }
+
+  const token = user.createJWT();
+  user.password = undefined;
+  res.status(201).json({ user, token });
+};
+
+module.exports = { register, login };
--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const bodyParser = require("body-parser");
 const port = process.env.PORT || 5000;
 const connectDB = require("./config/db");
 
 connectDB();
-
 const app = express();
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.use(bodyParser.json(), urlencodedParser);
+
 app.use(express.json());
-//app.use("/", require(""));
+app.get("/", (req, res) => {
+  res.send("Hello Mpdam");
+});
+app.use("/api/v1/auth", require("./routes/authRoutes"));
 app.listen(port, () => console.log(`Server Stated on ${port}`));
