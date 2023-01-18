bff890697b0dc44203808eb9b486d5ea81c87c93
WajdiGridha <wajdigridha744@gmail.com>
2023-01-18 01:15:08 +0100

    fix all controllers and routes Category Users Products

--- backend/routes/produitRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { AddProduit, UpdateProduit, DeleteProduit, GetAllProduit, GetProduit } = require("../controllers/produitController");
-const router = express.Router();
-
-router.route("/").post(AddProduit);
-router.route('/:id').delete(DeleteProduit).put(UpdateProduit).get(GetProduit);
-router.route("/").get(GetAllProduit);
-
-module.exports = router;
